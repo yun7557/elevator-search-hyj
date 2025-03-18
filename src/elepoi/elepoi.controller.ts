ad617a09f81080c1a2c2f9f7e648fd95dbdddbf6
yun7557 <yugi7557@gmail.com>
2025-03-18 10:41:20 +0900

    elpoi update v2

--- src/elepoi/elepoi.controller.ts
+++ src/elepoi/elepoi.controller.ts
@@ -1,18 +1,16 @@
 import { Controller, Get, Query } from "@nestjs/common";
 import { ElepoiService } from "./elepoi.service";
 import { Elepoi } from "./elepoi.entity";
-import { ElepoiNearbyDto } from "./elepoi-nearby-dto";
+import { ElepoiNearbyDto } from "./dto/elepoi-nearby-dto";
 
 @Controller("elepoi")
 export class ElepoiController {
     constructor(private readonly elepoiService: ElepoiService) {}
 
-
     @Get()
     async findAll(): Promise<Elepoi[]>{
         return this.elepoiService.findAll();
     }
-
 
     @Get('nearby')
     async findNearby(@Query() query: ElepoiNearbyDto): Promise<Elepoi[]> {
